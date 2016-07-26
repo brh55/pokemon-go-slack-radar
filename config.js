15ffdbdaf26099afdbb7a03c5812feeed3dd7961
Brandon Him <brh55@drexel.edu>
2016-07-26 00:38:13 -0500

    Add config opts for geocode

--- config.js
+++ config.js
@@ -4,16 +4,28 @@ var dotenv = require('dotenv');
 
 dotenv.load();
 
-/**
- * Export .env config values into into a config object
- *
- * @type {Object}
- */
-module.exports = {
+var botConfigs = {
     webhook: process.env.SLACK_WEBHOOK,
     port: Number(process.env.PORT) || 5000,
     emoji: process.env.BOT_EMOJI || 'robot_face',
     username: process.env.BOT_USERNAME || 'Mr_SlackBot',
     channel: '#' + process.env.SLACK_CHANNEL || '#default',
     updateColor: process.env.UPDATE_COLOR || '#27ae60'
+};
+
+var geocoderConfigs = {
+  provider: 'google',
+  httpAdapter: 'https', // Default 
+  apiKey: process.env.GOOGLE_API_KEY || null,
+  formatter: null         // 'gpx', 'string', ... 
+};
+
+/**
+ * Export .env config values into into a config object
+ *
+ * @type {Object}
+ */
+module.exports = {
+    bot: botConfigs,
+    geocoder: geocodeConfigs
 };
