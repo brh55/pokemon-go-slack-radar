2c7b88fe35de71bb3ff418cbf6bd6468636b2ee0
Brandon Him <brh55@drexel.edu>
2016-07-26 02:06:13 -0500

    Tidy up service for use, and add libs

--- libs/slackService.js
+++ libs/slackService.js
@@ -16,13 +16,34 @@ module.exports = (function () {
     /**
      * Slack Wrapper to send Slack message
      *
+     * @param  {object) config containing response url and message
      * @param  {object} message object message to be send
      */
-    var sendMessage = function (message) {
+    var sendMessage = function (url, message) {
+        slack = new Slack(url);
         slack.send(message);
     };
 
+    /**
+     * Build response config based on payload of recieved message
+     * @param  {object} requestBody body of payload
+     * @return {object}             configs for message (channel, response_url)
+     */
+    var parseHook = function (body) {
+        return {
+            channelId: body.channel_id,
+            channel: "#" + body.channel_name,
+            domain: body.team_domain,
+            user: body.user_name,
+            teamId: body.team_id,
+            token: body.token,
+            url: body.response_url,
+            userId: body.user_id
+        }
+    };
+
     return {
-        sendMessage: sendMessage
+        sendMessage: sendMessage,
+        parseHook: parseHook
     };
 })();
