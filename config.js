29833bc9855a6d1ed58944e9ddf76d6a6c65e842
Brandon Him <brh55@drexel.edu>
2016-07-26 02:04:13 -0500

    Strip config for useful stuff only

--- config.js
+++ config.js
@@ -5,12 +5,10 @@ var dotenv = require('dotenv');
 dotenv.load();
 
 var botConfigs = {
-    webhook: process.env.SLACK_WEBHOOK,
+    webhook: 'https://hooks.slack.com/commands/T07PN3GQZ/63124742209/tQsGUUnReSDMGtTFqh9',
     port: Number(process.env.PORT) || 5000,
-    emoji: process.env.BOT_EMOJI || 'robot_face',
-    username: process.env.BOT_USERNAME || 'Mr_SlackBot',
-    channel: '#' + process.env.SLACK_CHANNEL || '#default',
-    updateColor: process.env.UPDATE_COLOR || '#27ae60'
+    updateColor: process.env.UPDATE_COLOR || '#27ae60',
+    slackToken: process.env.SLACK_TOKEN
 };
 
 var geocoderConfigs = {
@@ -27,5 +25,5 @@ var geocoderConfigs = {
  */
 module.exports = {
     bot: botConfigs,
-    geocoder: geocodeConfigs
+    geocoder: geocoderConfigs
 };
