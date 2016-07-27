c4916233f7605037360a3b8f8471332ee80ec735
Brandon Him <brh55@drexel.edu>
2016-07-27 08:36:15 -0500

    Server.js file tidy

--- server.js
+++ server.js
@@ -4,17 +4,20 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
-var token = require('./config').bot.slackToken;
+var config = require('./config');
+
+var token = config.bot.slackToken;
+
+var Promise = require("bluebird");
+var bodyParser = require('body-parser');
 
 // Libs
-var bodyParser = require('body-parser');
 var util = require('./libs/util');
 var slackService = require('./libs/slackService');
 var decodeService = require('./libs/decodeService');
-// var pokeService = require('./libs/pokeService');
-var Promise = require("bluebird");
 var pokeScan = Promise.promisify(require('pokego-scan'));
 
+// Routes
 var debugRoute = require('./routes/debug');
 
 // Commonly used Middleware to parse JSON request
@@ -22,6 +25,7 @@ var urlEncodeParser = bodyParser.urlencoded({extended: true});
 
 app.post('/', urlEncodeParser, function (req, res) {
     // if request doesn't contain body, respond with 400 error.
+    if (!req.body) res.sendStatus(400);
 
     // Assuming that the user wants to ensure
     // request is originating from own server
@@ -29,22 +33,19 @@ app.post('/', urlEncodeParser, function (req, res) {
         var request = slackService.parseHook(req.body);
         var input = req.body.text;
 
+        var message = {};
+        message.channel = request.channel;
+
         // If the address field is empty, inform user
         if (!input) {
-            slackService.sendMessage(
-                request.url,
-                {
-                    channel: request.channel,
-                    text: request.user + ', looks like you forgot to include an address.'
-                }
-            );
+            message.text = request.user + ', looks like you forgot to include an address.';
 
+            slackService.sendMessage(request.url, message);
             res.sendStatus(200);
         } else {
             // Decode and Send Pokemon Information
             decodeService.getAddress(input, function (err, result) {
                 if (err) res.sendStatus(400);
-                var message = '';
 
                 if (result.length === 1) {
                     var correctAddress = result[0].formattedAddress;
@@ -54,54 +55,59 @@ app.post('/', urlEncodeParser, function (req, res) {
                     };
 
                     pokeScan(coordinates).then(function(pokemonArray) {
-                        console.log(pokemonArray.length > 1);
                         var fields = [];
                         var attachments = [];
-                        var message = {};
+
                         message.text = 'Scanned ' + correctAddress;
+
                         for (var i = 0; i < pokemonArray.length; i++) {
                             var pokemon = pokemonArray[i];
-                            var mediaUrl = 'http://pokeapi.co/media/img/' + pokemonArray[i].pokemonId + '.png';
-                            var gmapUrl = 'https://www.google.com/maps/search/' + pokemon.latitude + '+' + pokemon.longitude;
+                            var mediaUrl = 'http://pokeapi.co/media/img/'
+                                            + pokemonArray[i].pokemonId + '.png';
+                            var gmapUrl = 'https://www.google.com/maps/search/' 
+                                            + pokemon.latitude + '+' 
+                                            + pokemon.longitude;
 
                             var attachmentObj = {
-                                fallback: pokemon.name + ' is ' + pokemon.distance_str + ' away and despawns in ' + pokemon.despawns_in_str,
+                                fallback: pokemon.name + ' is ' + pokemon.distance_str 
+                                    + ' away and despawns in ' 
+                                    + pokemon.despawns_in_str,
                                 title: pokemon.name,
                                 title_link: gmapUrl,
                                 thumb_url: mediaUrl,
                                 fields: [],
-                                color:'#fbd478'
+                                color: config.bot.updateColor
                             };
 
-                            attachmentObj.fields.push(slackService.buildField("Distance", pokemon.distance_str, true));
-                            attachmentObj.fields.push(slackService.buildField("Despawns In", pokemon.despawns_in_str + " mins", true));
-                            attachments.push(attachmentObj);
+                            attachmentObj.fields
+                                .push(slackService.buildField("Distance",
+                                                            pokemon.distance_str,
+                                                            true));
+                            attachmentObj.fields
+                                .push(slackService.buildField("Despawns In",
+                                                            pokemon.despawns_in_str + " mins",
+                                                            true));
+                            attachments
+                                .push(attachmentObj);
                         }
 
                         message.attachments = attachments;
 
-
-                        slackService.sendMessage(
-                            request.url,
-                            message
-                        );
-
+                        slackService.sendMessage(request.url, message);
                         res.sendStatus(200);
                     })
                     .catch(function(err) {
-                        slackService.sendMessage(
-                            request.url,
-                            {
-                                channel: request.channel,
-                                text: ':cry: Sorry, there is a issue with the Pokévision servers.',
-                            }
-                        );
+                        message.text = ':cry: Sorry, there is a issue with the Pokévision servers.';
+
+                        slackService.sendMessage(request.url, message);
                         res.sendStatus(200);
                         return;
                     });
                 } else if (result.length > 0) {
                     // More than one address found
-                    var preText = 'Which ' + input + ' did you mean?'
+                    message.text = 'Which ' + input + ' did you mean?'
+                    message.attachments = [];
+                    var attachmentObj = {};
 
                     // @TODO - Figure out interactivity with buttons
                     // to respond with
@@ -110,29 +116,19 @@ app.post('/', urlEncodeParser, function (req, res) {
                         var numSelection = i + 1;
                         var tmpString = '\n' + numSelection + '. ' + address;
 
-                        message += tmpString;
+                        attachmentObj.fallback += tmpString;
                     }
-                    slackService.sendMessage(
-                        request.url,
-                        {
-                            channel: request.channel,
-                            text: message,
-                        }
-                    );
 
+                    message.attachments.push(attachmentObj);
+
+                    slackService.sendMessage(request.url, message);
                     res.sendStatus(200);
                 } else {
-                    // Nothing was found
-                    slackService.sendMessage(
-                        request.url,
-                        {
-                            channel: request.channel,
-                            text: 'The address entered was not found',
-                        }
-                    );
+                    // No address found
+                    message.text = 'The address entered was not found';
+                    slackService.sendMessage(request.url, messsage);
                 }
             });
-
         }
     }
 });
