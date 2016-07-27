1511397fc9457e465ddff617e02fbcec0840a177
Brandon Him <brh55@drexel.edu>
2016-07-27 08:49:34 -0500

    Rewrite utils to fit style

--- libs/util.js
+++ libs/util.js
@@ -3,69 +3,67 @@
 var nodeUtil = require('util');
 
 module.exports = (function () {
-    var action = {
-        /**
-         * Format date of ISO Date
-         *
-         * @param  {string} ISODate ISODate format
-         * @return {string}         returns in format of: MonthDate, Hour:Mins PM / AM
-         */
-        formatDate: function (ISODate) {
-            var tempDate = ISODate.replace(/T/, ' ').replace(/\..+/, '');
-            var dateSplit = tempDate.split(' ');
+    /**
+     * Format date of ISO Date
+     *
+     * @param  {string} ISODate ISODate format
+     * @return {string}         returns in format of: MonthDate, Hour:Mins PM / AM
+     */
+    var formatDate = function (ISODate) {
+        var tempDate = ISODate.replace(/T/, ' ').replace(/\..+/, '');
+        var dateSplit = tempDate.split(' ');
 
-            var monthDate = dateSplit[0];
-            var time = dateSplit[1];
-            var timeSplit = time.split(':');
+        var monthDate = dateSplit[0];
+        var time = dateSplit[1];
+        var timeSplit = time.split(':');
 
-            var hour = timeSplit[0];
-            var mins = timeSplit[1];
+        var hour = timeSplit[0];
+        var mins = timeSplit[1];
 
-            // Format into 12 hour format
-            var hours = ((hour + 11) % 12 + 1);
-            var suffix = (hours >= 12) ? 'PM' : 'AM';
+        // Format into 12 hour format
+        var hours = ((hour + 11) % 12 + 1);
+        var suffix = (hours >= 12) ? 'PM' : 'AM';
 
-            var formattedDate = monthDate + ' ' + hours + ':' + mins + ' ' + suffix + ' UTC';
+        var formattedDate = monthDate + ' ' + hours + ':' + mins + ' ' + suffix + ' UTC';
 
-            return formattedDate;
-        },
+        return formattedDate;
+    };
 
-        /**
-         * Checks if all keys do not have undefined values string values
-         *
-         * @param  {object} object object in question
-         * @return {boolean}        return if object contains undefined string values
-         */
-        allDefined: function (object) {
-            for (var key in object) {
-                if (nodeUtil.isUndefined(object[key])) {
-                    return false;
-                }
+    /**
+     * Checks if all keys do not have undefined values string values
+     *
+     * @param  {object} object object in question
+     * @return {boolean}        return if object contains undefined string values
+     */
+    var allDefined = function (object) {
+        for (var key in object) {
+            if (nodeUtil.isUndefined(object[key])) {
+                return false;
             }
-            return true;
-        },
-
-        /**
-         * Return undefined keys of object
-         *
-         * @param  {object} object object in question
-         * @return {string}        string of undefined valued keys
-         */
-        getUndefinedKeys: function (object, formatSeparator) {
-            var undefKeys = [];
-            for (var key in object) {
-                if (nodeUtil.isUndefined(object[key])) {
-                    undefKeys.push(key);
-                }
-            }
-
-            return undefKeys.toString().replace(/,/g, formatSeparator);
         }
+        return true;
+    };
+
+    /**
+     * Return undefined keys of object
+     *
+     * @param  {object} object object in question
+     * @return {string}        string of undefined valued keys
+     */
+    var getUndefinedKeys = function (object, formatSeparator) {
+        var undefKeys = [];
+        for (var key in object) {
+            if (nodeUtil.isUndefined(object[key])) {
+                undefKeys.push(key);
+            }
+        }
+
+        return undefKeys.toString().replace(/,/g, formatSeparator);
     };
 
     return {
-        formatDate: action.formatDate,
-        allDefined: action.allDefined,
-        getUndefinedKeys: action.getUndefinedKeys
+        formatDate: formatDate,
+        allDefined: allDefined,
+        getUndefinedKeys: getUndefinedKeys
     };
 })();
